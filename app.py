4e6873702040e5bb54f47b02fcdfa5fa24ec6940
mcbulan22 <113360257+mcbulan22@users.noreply.github.com>
2025-07-01 22:39:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -33,8 +33,7 @@ col1, col2 = st.columns([1, 5])
 with col1:
     st.image(logo, width=100)
 with col2:
-    st.markdown("## 📊 Student Assessment Performance Dashboard")
-    st.markdown("### _Center for Competency Assessment_")
+    st.markdown("## CCA Student Assessment Performance Dashboard")
 
 tab1, tab2 = st.tabs(["👤 Individual View", "👥 Group View"])
 
